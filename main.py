a548a831e419d8cc2e4e06355f3e3838dc53d0bd
Ben <benjaminzheng@college.harvard.edu>
2015-07-30 09:58:20 -0400

    Began writin main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import itertools
 import random
 import copy
 import numpy as np
-from Cell import *
+from cell import *
+from pydispatch import dispatcher
 
-class Schelling:
-    def __init__(self, width, height, empty_ratio, similarity_threshold, n_iterations, races = 2):
+class Colon:
+    def __init__(self, layers, width, height, inner_width, inner_height, healthy_ratio, empty_ratio):
+        self.layers = layers
         self.width = width
         self.height = height
-        self.races = races
-        self.empty_ratio = empty_ratio
-        self.similarity_threshold = similarity_threshold
-        self.n_iterations = n_iterations
+        self.inner_width = inner_width
+        self.inner_height = inner_height
+        self.healthy_ratio = healthy_ratio
         self.empty_houses = []
         self.agents = {}
 
@@ -33,12 +34,79 @@ class Schelling:
             )
 
     def is_unsatisfied(self, x, y):
+        race = self.agents[(x,y)]
+        count_similar = 0
+        count_different = 0
 
+        if x > 0 and y > 0 and (x-1, y-1) not in self.empty_houses:
+            if self.agents[(x-1, y-1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1
+        if y > 0 and (x,y-1) not in self.empty_houses:
+            if self.agents[(x,y-1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1
+        if x < (self.width-1) and y > 0 and (x+1,y-1) not in self.empty_houses:
+            if self.agents[(x+1,y-1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1
+        if x > 0 and (x-1,y) not in self.empty_houses:
+            if self.agents[(x-1,y)] == race:
+                count_similar += 1
+            else:
+                count_different += 1        
+        if x < (self.width-1) and (x+1,y) not in self.empty_houses:
+            if self.agents[(x+1,y)] == race:
+                count_similar += 1
+            else:
+                count_different += 1
+        if x > 0 and y < (self.height-1) and (x-1,y+1) not in self.empty_houses:
+            if self.agents[(x-1,y+1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1        
+        if x > 0 and y < (self.height-1) and (x,y+1) not in self.empty_houses:
+            if self.agents[(x,y+1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1        
+        if x < (self.width-1) and y < (self.height-1) and (x+1,y+1) not in self.empty_houses:
+            if self.agents[(x+1,y+1)] == race:
+                count_similar += 1
+            else:
+                count_different += 1
+
+        if (count_similar+count_different) == 0:
+            return False
+        else:
+            return float(count_similar)/(count_similar+count_different) < self.happy_threshold
 
     def update(self):
-
+        for i in range(self.n_iterations):
+        self.old_agents = copy.deepcopy(self.agents)
+        n_changes = 0
+        for agent in self.old_agents:
+            if self.is_unhappy(agent[0], agent[1]):
+                agent_race = self.agents[agent]
+                empty_house = random.choice(self.empty_houses)
+                self.agents[empty_house] = agent_race
+                del self.agents[agent]
+                self.empty_houses.remove(empty_house)
+                self.empty_houses.append(agent)
+                n_changes += 1
+        print n_changes
+        if n_changes == 0:
+            break
 
     def move_to_empty(self, x, y):
-
+        race = self.agents[(x,y)]
+    empty_house = random.choice(self.empty_houses)
+    self.updated_agents[empty_house] = race
+    del self.updated_agents[(x, y)]
+    self.empty_houses.remove(empty_house)
+    self.empty_houses.append((x, y))
 
     def plot(self):
