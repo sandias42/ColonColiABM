8f549f5e0b5d76e70f019e3b2164a4c459798759
Ben <benjaminzheng@college.harvard.edu>
2015-07-31 11:03:33 -0400

    more colon intro fixes

--- xrangeTest.py
+++ xrangeTest.py
@@ -1,7 +1,7 @@
 import itertools
 
 l = list(itertools.product(range(10),range(5)))
-m = list(itertools.product(xrange(10),xrange(5)))
+m = list(itertools.product(xrange(10),xrange(5)))+list(itertools.product(xrange(1),xrange(2)))
 for i in itertools.islice(m, 49, None):
 	if l[i[0]] == m[i[0]]:
 		print("Match")
